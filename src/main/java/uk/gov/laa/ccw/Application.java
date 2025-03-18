35fbc03d8412eed9b16ef1762a646e0c324e8b0c
Chris Cook <christopher.cook@digital.justice.gov.uk>
2025-03-18 14:48:13 +0000

    add Spring Boot

---
+++ src/main/java/uk/gov/laa/ccw/Application.java
@@ -0,0 +1,18 @@
+package uk.gov.laa.ccw;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        log.info("Hi Jason");
+
+        SpringApplication.run(Application.class, args);
+    }
+
+}
