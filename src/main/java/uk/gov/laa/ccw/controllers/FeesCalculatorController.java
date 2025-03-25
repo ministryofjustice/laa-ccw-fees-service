f8a6e8762c9bccccaee9a04389a5a669261ac33c
Chris Cook <christopher.cook@digital.justice.gov.uk>
2025-03-25 13:34:51 +0000

    BILL-62 - add basic endpoint that just doubles a number

---
+++ src/main/java/uk/gov/laa/ccw/controllers/FeesCalculatorController.java
@@ -0,0 +1,36 @@
+package uk.gov.laa.ccw.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.DecimalFormat;
+
+import static org.springframework.http.ResponseEntity.badRequest;
+
+@Slf4j
+@RestController
+public class FeesCalculatorController {
+
+    @PostMapping("/v1/fees/{number}")
+    String doubleANumber(@PathVariable(value = "number") String number) {
+        log.info("Supplied value is {}", number);
+        float numberAsFloat = Float.parseFloat(number);
+        DecimalFormat currencyFormat = new DecimalFormat("###0.00");
+        return currencyFormat.format(numberAsFloat * 2);
+    }
+
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ResponseEntity<String> shouldBeANumber(NumberFormatException exception) {
+        String message = "Should be supplied a number, but instead was supplied " + exception.getMessage();
+        log.error(message);
+        return badRequest().body(message);
+    }
+
+}
