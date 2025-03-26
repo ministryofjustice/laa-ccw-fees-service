c28a374134025f242b7c5d07f936ffcaa182149f
jason smallman <jason.smallman@digital.justice.gov.uk>
2025-03-26 16:34:57 +0000

    add new tables to database

---
+++ src/main/java/uk/gov/laa/ccw/Services/DataAccessUtilities.java
@@ -0,0 +1,36 @@
+package uk.gov.laa.ccw.Services;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class DataAccessUtilities {
+    private static final String ERROR_MESSAGE_FORMAT = "Unable to read file with filePath [%s]";
+
+    public static void initialiseDatabase(JdbcTemplate jdbcTemplate) {
+        String sqlSchema = readResourceToString("ccw_database_schema.sql");
+        String sqlData = readResourceToString("ccw_database_data.sql");
+
+        jdbcTemplate.execute(sqlSchema);
+        jdbcTemplate.execute(sqlData);
+
+    }
+
+    public static String readResourceToString(String filePath) {
+
+        ClassLoader classLoader = DataAccessUtilities.class.getClassLoader();
+        URL path = classLoader.getResource(filePath);
+        if (path == null) {
+            throw new RuntimeException(String.format(ERROR_MESSAGE_FORMAT, filePath));
+        }
+
+        File file = new File(path.getFile());
+        try {
+            return org.apache.commons.io.FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException(String.format(ERROR_MESSAGE_FORMAT, filePath));
+        }
+    }
+}
