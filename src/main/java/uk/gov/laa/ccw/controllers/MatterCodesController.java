0b711e73ea673a47517262ff110a4af4106cdea2
jason smallman <jason.smallman@digital.justice.gov.uk>
2025-03-31 16:25:40 +0100

    tidied up code

--- src/main/java/uk/gov/laa/ccw/controllers/MatterCodesController.java
+++ src/main/java/uk/gov/laa/ccw/controllers/MatterCodesController.java
@@ -3,6 +3,8 @@ package uk.gov.laa.ccw.controllers;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.laa.ccw.mapping.MatterCodesResponseMapping;
 import uk.gov.laa.ccw.models.MatterCodes200Response;
