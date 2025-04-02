d90072799f82a731cc9eae4ab1f3ebec93c357a2
Tosin <tosin.yusuf@digital.justice.gov.uk>
2025-04-02 14:34:39 +0100

    BILL-88 Added datasource for PostgreSQL db & actuator

--- src/main/java/uk/gov/laa/ccw/Application.java
+++ src/main/java/uk/gov/laa/ccw/Application.java
@@ -2,12 +2,16 @@ package uk.gov.laa.ccw;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 import uk.gov.laa.ccw.services.DataAccessUtilities;
 
+/**
+ * Entry point for the Fees Calculator Service application.
+ */
 @Slf4j
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -15,13 +19,28 @@ public class Application implements CommandLineRunner {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+    @Value("${spring.profiles.active:}")
+    private String activeProfile;
+
+    /**
+     * The application main method.
+     *
+     * @param args the application arguments.
+     */
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
     }
 
+    /**
+     * The application run method.
+     *
+     * @param strings the application arguments.
+     */
     @Override
-    public void run(String... strings) throws Exception {
-        DataAccessUtilities.initialiseDatabase(jdbcTemplate);
+    public void run(String... strings) {
+        if (activeProfile.equals("local")) {
+            DataAccessUtilities.initialiseDatabase(jdbcTemplate);
+        }
     }
 }
