3f826b262596189127f6e119320171ed7a15f7e9
Tosin <tosin.yusuf@digital.justice.gov.uk>
2025-04-03 16:53:02 +0100

    BILL-95: Updated test

--- src/test/java/uk/gov/laa/ccw/mapper/dao/MatterCodeMapperTest.java
+++ src/test/java/uk/gov/laa/ccw/mapper/dao/MatterCodeMapperTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.laa.ccw.entity.MatterCodesOneEntity;
+import uk.gov.laa.ccw.entity.MatterCodesTwoEntity;
 import uk.gov.laa.ccw.models.MatterCode;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,7 +17,7 @@ class MatterCodeMapperTest {
     private MatterCodeMapper matterCodeMapper = new MatterCodeMapperImpl();
 
     @Test
-    void shouldMapToItemEntity() {
+    void shouldMapMatterCodesOneEntityToMatterCode() {
         MatterCodesOneEntity matterCodesOneEntity = MatterCodesOneEntity.builder()
                 .matterCodeId("mt1")
                 .build();
@@ -25,6 +26,18 @@ class MatterCodeMapperTest {
         assertNotNull(result);
         assertEquals("mt1", result.getMatterCodeId(), matterCodesOneEntity.getMatterCodeId());
         assertEquals("", result.getDescription(), matterCodesOneEntity.getDescription());
+    }
+
+    @Test
+    void shouldMapMatterCodesTwoEntityToMatterCode() {
+        MatterCodesTwoEntity matterCodesTwoEntity = MatterCodesTwoEntity.builder()
+                .matterCodeId("mt2")
+                .build();
+        MatterCode result = matterCodeMapper.toMatterCode(matterCodesTwoEntity);
+
+        assertNotNull(result);
+        assertEquals("mt2", result.getMatterCodeId(), matterCodesTwoEntity.getMatterCodeId());
+        assertEquals("", result.getDescription(), matterCodesTwoEntity.getDescription());
     }
 
 }
