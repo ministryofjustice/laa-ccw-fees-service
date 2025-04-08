30735da104d36ea001f1002cde74a1410d6e645e
Tosin <tosin.yusuf@digital.justice.gov.uk>
2025-04-08 15:30:52 +0100

    BILL-113 Removed Dao classes

--- src/test/java/uk/gov/laa/ccw/services/CaseStagesServiceTest.java
+++ src/test/java/uk/gov/laa/ccw/services/CaseStagesServiceTest.java
@@ -5,54 +5,39 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import uk.gov.laa.ccw.dao.CaseStagesDao;
-import uk.gov.laa.ccw.model.CaseStage;
+import uk.gov.laa.ccw.entity.CaseStagesEntity;
 import uk.gov.laa.ccw.exceptions.CaseStagesNotFoundException;
+import uk.gov.laa.ccw.mapper.dao.CaseStagesMapper;
+import uk.gov.laa.ccw.model.CaseStage;
+import uk.gov.laa.ccw.repository.CaseStagesRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class CaseStagesServiceTest {
     @Mock
-    private CaseStagesDao caseStagesDao;
+    private CaseStagesRepository caseStagesRepository;
+
+    @Mock
+    private CaseStagesMapper caseStagesMapper;
 
     @InjectMocks
     private CaseStagesService classUnderTest;
 
-    private List<CaseStage> setupData(Boolean addInFPL10) {
-        List<CaseStage> dataSet = new ArrayList<CaseStage>();
-        CaseStage rowSet;
-        if (addInFPL10) {
-            rowSet = CaseStage.builder()
-                    .caseStageId("FPL10")
-                    .build();
-            dataSet.add(rowSet);
-        }
-        rowSet = CaseStage.builder()
-                .caseStageId("cs1")
-                .build();
-        dataSet.add(rowSet);
-        rowSet = CaseStage.builder()
-                .caseStageId("cs2")
-                .build();
-        dataSet.add(rowSet);
-        return dataSet;
-    }
-
     @Test
     void shouldFetchCaseStagesForMatterCodeOneAndMatterCodeTwo() {
 
-        when(caseStagesDao.fetchCaseStages(anyString()))
-                .thenReturn(setupData(false));
+        String matterCodeOne = "CODE1";
+        String matterCodeTwo = "CODE2";
 
-        List<CaseStage> dataReturned = classUnderTest.getAllCaseStagesForMatterCodes("CODE1", "CODE2");
+        setUpMockData(matterCodeOne, false);
+
+        List<CaseStage> dataReturned = classUnderTest.getAllCaseStagesForMatterCodes(matterCodeOne, matterCodeTwo);
         assertEquals(2, dataReturned.size());
         assertEquals("cs1", dataReturned.get(0).getCaseStageId());
         assertEquals("cs2", dataReturned.get(1).getCaseStageId());
@@ -61,10 +46,12 @@ public class CaseStagesServiceTest {
     @Test
     void shouldFetchCaseStagesForMatterCodeOneAndMatterCodeTwoWithFPL10() {
 
-        when(caseStagesDao.fetchCaseStages(anyString()))
-                .thenReturn(setupData(true));
+        String matterCodeOne = "CODE1";
+        String matterCodeTwo = "CODE2";
 
-        List<CaseStage> dataReturned = classUnderTest.getAllCaseStagesForMatterCodes("CODE1", "CODE2");
+        setUpMockData(matterCodeOne, true);
+
+        List<CaseStage> dataReturned = classUnderTest.getAllCaseStagesForMatterCodes(matterCodeOne, matterCodeTwo);
         assertEquals(2, dataReturned.size());
         assertEquals("cs1", dataReturned.get(0).getCaseStageId());
         assertEquals("cs2", dataReturned.get(1).getCaseStageId());
@@ -73,10 +60,12 @@ public class CaseStagesServiceTest {
     @Test
     void shouldFetchCaseStagesForFAMA() {
 
-        when(caseStagesDao.fetchCaseStages(anyString()))
-                .thenReturn(setupData(true));
+        String matterCodeOne = "FAMA";
+        String matterCodeTwo = "CODE2";
 
-        List<CaseStage> dataReturned = classUnderTest.getAllCaseStagesForMatterCodes("FAMA", "CODE2");
+        setUpMockData(matterCodeOne, true);
+
+        List<CaseStage> dataReturned = classUnderTest.getAllCaseStagesForMatterCodes(matterCodeOne, matterCodeTwo);
         assertEquals(2, dataReturned.size());
         assertEquals("cs1", dataReturned.get(0).getCaseStageId());
         assertEquals("cs2", dataReturned.get(1).getCaseStageId());
@@ -85,10 +74,12 @@ public class CaseStagesServiceTest {
     @Test
     void shouldFetchCaseStagesForFPET() {
 
-        when(caseStagesDao.fetchCaseStages(anyString()))
-                .thenReturn(setupData(true));
+        String matterCodeOne = "CODE1";
+        String matterCodeTwo = "FPET";
 
-        List<CaseStage> dataReturned = classUnderTest.getAllCaseStagesForMatterCodes("CODE1", "FPET");
+        setUpMockData(matterCodeOne, true);
+
+        List<CaseStage> dataReturned = classUnderTest.getAllCaseStagesForMatterCodes(matterCodeOne, matterCodeTwo);
         assertEquals(2, dataReturned.size());
         assertEquals("cs1", dataReturned.get(0).getCaseStageId());
         assertEquals("cs2", dataReturned.get(1).getCaseStageId());
@@ -97,20 +88,47 @@ public class CaseStagesServiceTest {
     @Test
     void shouldFetchCaseStagesForFAMAAndFPET() {
 
-        when(caseStagesDao.fetchCaseStages(anyString()))
-                .thenReturn(setupData(true));
+        String matterCodeOne = "FAMA";
+        String matterCodeTwo = "FPET";
 
-        List<CaseStage> dataReturned = classUnderTest.getAllCaseStagesForMatterCodes("FAMA", "FPET");
+        setUpMockData(matterCodeOne, true);
+
+        List<CaseStage> dataReturned = classUnderTest.getAllCaseStagesForMatterCodes(matterCodeOne, matterCodeTwo);
         assertEquals(3, dataReturned.size());
-        assertEquals("FPL10", dataReturned.get(0).getCaseStageId());
+        assertEquals("cs1", dataReturned.get(0).getCaseStageId());
+        assertEquals("cs2", dataReturned.get(1).getCaseStageId());
+        assertEquals("FPL10", dataReturned.get(2).getCaseStageId());
     }
 
     @Test
-    void shouldThrowExceptionWhenDaoThrowsDatabaseReadException() {
+    void shouldThrowExceptionWhenCaseStagesNotFound() {
 
-        doThrow(new CaseStagesNotFoundException(""){}).when(caseStagesDao).fetchCaseStages(anyString());
+        String matterCodeOne = "CODE1";
+        String matterCodeTwo = "CODE2";
+
+        when(caseStagesRepository.findCaseStagesByMatterCodeOne(matterCodeOne)).thenReturn(new ArrayList<>());
 
         assertThrows(CaseStagesNotFoundException.class,
-                () -> classUnderTest.getAllCaseStagesForMatterCodes("CODE1", "CODE2"));
+                () -> classUnderTest.getAllCaseStagesForMatterCodes(matterCodeOne, matterCodeTwo));
     }
+
+    private void setUpMockData(String matterCodeOne, boolean addInFPL10) {
+        CaseStagesEntity cs1Entity = CaseStagesEntity.builder().caseStageId("cs1").build();
+        CaseStagesEntity cs2Entity = CaseStagesEntity.builder().caseStageId("cs2").build();
+        List<CaseStagesEntity> caseStagesEntities = new ArrayList<>();
+        caseStagesEntities.add(cs1Entity);
+        caseStagesEntities.add(cs2Entity);
+
+        when(caseStagesMapper.toCaseStage(cs1Entity)).thenReturn(CaseStage.builder().caseStageId("cs1").build());
+        when(caseStagesMapper.toCaseStage(cs2Entity)).thenReturn(CaseStage.builder().caseStageId("cs2").build());
+
+        if (addInFPL10) {
+            CaseStagesEntity fpl10Entity = CaseStagesEntity.builder().caseStageId("FPL10").build();
+            caseStagesEntities.add(fpl10Entity);
+            when(caseStagesMapper.toCaseStage(fpl10Entity)).thenReturn(CaseStage.builder().caseStageId("FPL10").build());
+        }
+
+        when(caseStagesRepository.findCaseStagesByMatterCodeOne(matterCodeOne)).thenReturn(caseStagesEntities);
+    }
+
 }
