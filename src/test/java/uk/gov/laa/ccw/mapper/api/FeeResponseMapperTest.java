827e71fe578d81b057e94c9e18a6e40489614a51
jason smallman <jason.smallman@digital.justice.gov.uk>
2025-04-14 15:30:57 +0100

    restructured fee calculation response

--- src/test/java/uk/gov/laa/ccw/mapper/api/FeeResponseMapperTest.java
+++ src/test/java/uk/gov/laa/ccw/mapper/api/FeeResponseMapperTest.java
@@ -2,11 +2,9 @@ package uk.gov.laa.ccw.mapper.api;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import uk.gov.laa.ccw.model.FeeTotals;
-import uk.gov.laa.ccw.model.api.FeeCalculate200Response;
-import uk.gov.laa.ccw.model.api.FeeCalculateRequest;
+import uk.gov.laa.ccw.model.FeeElement;
+import uk.gov.laa.ccw.model.api.FeeCalculate200ResponseFee;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -14,29 +12,20 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @ExtendWith(MockitoExtension.class)
 public class FeeResponseMapperTest {
 
-    @InjectMocks
-    private FeeResponseMapper classUnderTest;
+    private final FeeResponseMapper feeRespMapper = new FeeResponseMapperImpl();
 
     @Test
     void shouldMapToFeeResponse() {
-        FeeTotals fee = FeeTotals.builder()
-                .amount(12.34)
-                .vat(2.56)
-                .total(233.45)
-                .build();
-        FeeCalculateRequest feeRequest = FeeCalculateRequest.builder()
-                .caseStage("caseStage")
-                .matterCode1("matterCode1")
-                .matterCode2("matterCode2")
-                .locationCode("locationCode")
+        FeeElement fee = FeeElement.builder()
+                .amount("12.34")
+                .vat("2.56")
+                .total("233.45")
                 .build();
 
-        FeeCalculate200Response result = classUnderTest.toFeeCalculateResponse(
-                fee
-                );
+        FeeCalculate200ResponseFee result = feeRespMapper.toFeeCalculateResponse(fee);
 
         assertNotNull(result);
-        assertEquals("12.34", result.getTotals().getAmount());
-        assertEquals("2.56", result.getTotals().getVat());
+        assertEquals("12.34", result.getAmount());
+        assertEquals("2.56", result.getVat());
     }
 }
